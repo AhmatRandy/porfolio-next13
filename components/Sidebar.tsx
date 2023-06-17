9446c1beac0d72f8221d45586257b87d5852c3fd
Ahmat Randy <98681609+AhmatRandy@users.noreply.github.com>
2023-06-18 00:00:49 +0700

    styling

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { GoArrowDown } from "react-icons/go";
+import { AiOutlineWhatsApp } from "react-icons/ai";
 
 export default function Sidebar() {
   const name = "<AhmatRandy/>";
@@ -16,35 +19,39 @@ export default function Sidebar() {
         Download Resume
       </p>
 
-      <div>
-        {/* <a href="https://www.youtube.com/channel/UClW8d1f5m0QAE_Ig024EP6A">
-         <AiFillYoutube className="w-8 h-8 cursor-pointer" />
-       </a>
-       <a href="https://www.linkedin.com/in/sumit-dey-4a04431a9/">
-         <AiFillLinkedin className="w-8 h-8 cursor-pointer" />
-       </a>
-       <a href="https://www.instagram.com/_sumax__/">
-         <AiFillGithub className="w-8 h-8 cursor-pointer" />{" "}
-       </a> */}
+      <div className="flex flex-row justify-center items-center space-x-4 text-[#0000}">
+        <a
+          href="www.linkedin.com/in/
+ahmat-randy-1a306318b
+"
+        >
+          <AiFillLinkedin className="w-8 h-8 cursor-pointer" />
+        </a>
+        <a href="https://github.com/AhmatRandy">
+          <AiFillGithub className="w-8 h-8 cursor-pointer" />{" "}
+        </a>
       </div>
 
-      <div>
-        <div></div>
-        <p></p>
-        <p> </p>
+      <div className="bg-slate-300 text-base py-2 px-2 rounded-md">
+        <div className="flex flex-row space-x-1 justify-center items-center">
+          <GoArrowDown className="text-red" />
+          <p>Jakarta, Indonesia</p>
+        </div>
+        <p>randyahmat76@gmail.com</p>
+        <div className="flex flex-row space-x-1 justify-center items-center">
+          <AiOutlineWhatsApp className="text-green-500 w-5 h-5 cursor-pointer" />
+          <p> 081258351781</p>
+        </div>
       </div>
 
-      <button
-
-      // onClick={() => window.open("mailto:code.sumax@gmail.com")}
-      >
-        Email me
-      </button>
-      <button
-      //  onClick={changeTheme}
-      >
-        Toggle Theme
-      </button>
+      <div className="flex flex-col space-y-3 justify-center items-center ">
+        <button className="py-2 px-2 rounded-3xl w-8/12 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+          Email me
+        </button>
+        <button className="py-2 px-2 rounded-3xl w-8/12 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+          Dark Ui
+        </button>
+      </div>
     </div>
   );
 }
